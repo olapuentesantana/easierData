306c45e1618aaa86a50adacff164e956f9bd440a
Federico Marini <marinif@uni-mainz.de>
2021-08-19 19:00:40 +0200

    general info on the package also as official man page

---
+++ R/easierData-pkg.R
@@ -0,0 +1,15 @@
+#' easierData
+#'
+#' This experimentHub package provides access to an exemplary bladder cancer dataset
+#' with both processed RNA-seq data and information on response to ICB therapy
+#' publicly available via IMvigor210CoreBiologies package, and to internal data
+#' indispensable for the functional performance of easier package.
+#'
+#' @references Lapuente-Santana, Óscar, Maisa van Genderen, Peter A. J. Hilbers,
+#' Francesca Finotello, and Federica Eduati. 2021. Interpretable Systems Biomarkers
+#' Predict Response to Immune-Checkpoint Inhibitors. Patterns, 100293.
+#' https://doi.org/10.1016/j.patter.2021.100293.
+#'
+#' @name easierData-pkg
+#' @docType package
+NULL
