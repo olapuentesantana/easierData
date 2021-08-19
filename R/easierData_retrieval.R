fd1287ab09b5d181f77c3820fe7fe7509eea27e2
Federico Marini <marinif@uni-mainz.de>
2021-08-19 15:20:06 +0200

    Concept of how dataset could/should be accessed, with functions vs direct ehub accessors

--- R/easierData_retrieval.R
+++ R/easierData_retrieval.R
@@ -20,3 +20,26 @@ list_easierData <- function() {
   return(dataset_df)
 }
 
+
+#' Get the Mariathasan2018 PDL1 treatment data
+#'
+#' Obtain the ...TODO... from Mariathasan et al. (2018)
+#'
+#' @references Mariathasan et al., full ref TODO
+#'
+#' @return A \linkS4class{SummarizedExperiment} object with counts and TPM
+#' quantifications.
+#'
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'   se_mariathasan <- get_Mariathasan2018_PDL1_treatment()
+#' }
+get_Mariathasan2018_PDL1_treatment <- function() {
+  eh <- ExperimentHub::ExperimentHub()
+  message("Retrieving Mariathasan2018_PDL1_treatment dataset...")
+  ehub_data <- eh[["EH6677"]]
+  return(ehub_data)
+}
+
